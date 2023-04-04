7ef9257066ea3e02dddfdbf9cedfc6ddc3d1c4c2
souravbain <126683679+souravbain@users.noreply.github.com>
2023-04-05 02:07:39 +0600

    Add files via upload

---
+++ 1013.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a;
+int b;
+int c;
+scanf("%d %d %d",&a,&b,&c);
+if(a>=b && a>=c)
+{
+printf("%d eh o maior\n",a);
+}
+else if(b>=c)
+{
+printf("%d eh o maior\n",b);
+}
+else
+{
+printf("%d eh o maior\n",c);
+}
+return 0;
+}
