5f2feafeee1a7cca0bbeb37272c011ac61acd3da
souravbain <126683679+souravbain@users.noreply.github.com>
2023-04-05 02:02:39 +0600

    Add files via upload

---
+++ 1011.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+        double p,r;
+        p=3.14159;
+        scanf("%lf",&r);
+        double s=(4/3.0)*3.14159*(r*r*r);
+        printf("VOLUME = %.3lf\n",s);
+        return 0;
+
+
+
+
+}
